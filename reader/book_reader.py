4c3aa9544c0c4014b0b8b43f75739a33a172fdcd
lunahernandez <145288658+lunahernandez@users.noreply.github.com>
2024-10-13 22:55:40 +0100

    indexer_dict.py added to indexer_dict package

--- reader/book_reader.py
+++ reader/book_reader.py
@@ -1,32 +1,29 @@
 import re
 
 
-# TODO check regex
-
 def read_words(filepath):
     """Extract words from a TXT file and return a list of words."""
-    words = []
 
-    try:
-        with open(filepath, 'r', encoding='utf-8') as file:
-            content = file.read()
-            words = re.findall(r"[A-Za-z0-9]+(?:[-'][A-Za-z0-9]+)*", content.lower())
-    except UnicodeDecodeError:
+    # TODO check regex pattern. English words can accept "-" in the middle of the word
+    # TODO exclude stop words
+    words = []
+    encodings = ['utf-8', 'utf-8-sig', 'windows-1252', 'latin1']
+
+    for encoding in encodings:
         try:
-            with open(filepath, 'r', encoding='utf-8-sig') as file:
+            with open(filepath, 'r', encoding=encoding) as file:
                 content = file.read()
-                words = re.findall(r"[A-Za-z0-9]+(?:[-'][A-Za-z0-9]+)*", content.lower())
+                words = re.findall(r'\b\w+\b', content.lower())
+                return words
+        except UnicodeDecodeError:
+            print(f"Error decoding with {encoding}, trying next...")
+            continue
         except FileNotFoundError:
             print(f"Error: File {filepath} not found.")
             return words
         except Exception as e:
             print(f"Error: {e}")
             return words
-    except FileNotFoundError:
-        print(f"Error: File {filepath} not found.")
-        return words
-    except Exception as e:
-        print(f"Error: {e}")
-        return words
 
+    print("Error: Could not decode the file with any of the attempted encodings.")
     return words
