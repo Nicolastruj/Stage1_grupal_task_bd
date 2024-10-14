61c20aa1632ddbeb7b8551c03faff6301c618e51
Ivan Anikin <ivan.anikin@outlook.com>
2024-10-14 17:38:18 +0100

    updated get_paragraphs_from_positions

--- query_engine/searcher.py
+++ query_engine/searcher.py
@@ -40,27 +40,6 @@ def get_book_metadata(book_path):
 
     return title, author, language
 
-# def get_paragraphs_from_positions(book_path, positions):
-#     """Given the list of positions, extract the surrounding paragraphs."""
-#     paragraphs = []
-
-#     try:
-#         with open(book_path, 'r', encoding='utf-8') as file:
-#             text = file.read()
-
-#         # Split the book into paragraphs
-#         book_paragraphs = text.split('\n\n')
-#         book_text = text.replace("\n", "")  # Remove line breaks for easier position handling
-
-#         for position in positions:
-#             for paragraph in book_paragraphs:
-#                 if paragraph.lower().find(book_text[position:position + 20].lower()) != -1:
-#                     paragraphs.append(paragraph.strip())
-#                     break  # Stop after finding the first match for the position
-
-#     except FileNotFoundError:
-#         print(f"Book file not found: {book_path}")
-
 def get_paragraphs_from_positions(book_path, positions, search_word):
     """Given the list of positions, extract the surrounding paragraphs."""
     paragraphs = []
@@ -80,8 +59,6 @@ def get_paragraphs_from_positions(book_path, positions, search_word):
 
     except FileNotFoundError:
         print(f"Book file not found: {book_path}")
-
-    return paragraphs
 
     return paragraphs
 
@@ -123,7 +100,7 @@ def query_engine(word, book_folder="../Datamart_libros", index_folder="../books_
         title, author, language = get_book_metadata(book_path)
 
         # Extract the paragraphs where the word occurs
-        paragraphs = get_paragraphs_from_positions(book_path, positions)
+        paragraphs = get_paragraphs_from_positions(book_path, positions, word)
 
         if paragraphs:
             results.append({
