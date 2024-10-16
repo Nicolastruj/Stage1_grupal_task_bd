228e466fd0df0723fe70d36f8188557910db9b41
Ivan Anikin <ivan.anikin@outlook.com>
2024-10-16 20:22:17 +0100

    classroom searcher new metadata function

--- query_engine/searcher.py
+++ query_engine/searcher.py
@@ -128,21 +128,16 @@ def query_engine(word, book_folder="../Datamart_libros", index_folder="../books_
     return results
 
 
-def main():
-    while (True):
-        word = input("Enter a word to search for: ")
-        search_results = query_engine(word)
+while (True):
+    word = input("Enter a word to search for: ")
+    search_results = query_engine(word)
 
-        for result in search_results:
-            print(f"Title: {result['title']}")
-            print(f"Author: {result['author']}")
-            print(f"URL: {result['url']}")
-            print(f"Occurrences at positions: {len(result['paragraphs'])} occurrence(s)\n")
+    for result in search_results:
+        print(f"Title: {result['title']}")
+        print(f"Author: {result['author']}")
+        print(f"URL: {result['url']}")
+        print(f"Occurrences at positions: {len(result['paragraphs'])} occurrence(s)\n")
 
-            for paragraph in result['paragraphs']:
-                print(paragraph)
-                print("")
-
-
-if __name__ == "_main_":
-    main()
+        for paragraph in result['paragraphs']:
+            print(paragraph)
+            print("")
