36678beda1fd903efd49415969f6181e4b677979
ElisaBreeze <elisamabre@gmail.com>
2024-10-17 15:07:59 +0100

    Word Object data model translated

--- data_model/object_type/Word.py
+++ data_model/object_type/Word.py
@@ -6,7 +6,7 @@ class Word:
         self.dictionary = dictionary if dictionary is not None else {}
 
     def __str__(self):
-        # Create the representation of the informacion for the dictionary and the id_name
+        """Create the representation of the information for the dictionary and the id_name."""
         result = f"Word: {self.id_name}\nDictionary:\n"
         for key, values in self.dictionary.items():
             values_str = ', '.join(map(str, values))
@@ -14,7 +14,7 @@ class Word:
         return result
 
     def to_dict(self):
-        """Convert the object to a dictionary for JSON serialization.."""
+        """Convert the object to a dictionary for JSON serialization."""
         return {
             'id_name': self.id_name,
             'dictionary': self.dictionary
@@ -22,7 +22,7 @@ class Word:
 
     @staticmethod
     def from_dict(data):
-        """Crear un objeto Palabra a partir de un diccionario."""
+        """Creat un Word object from a dictionary."""
         return Word(
             id_name=data['id_name'],
             dictionary=data['dictionary']
